c56d4d43a63e23543725e4442cd33561b770dd15
Jason Watson <jason.jw@live.co.uk>
2020-04-06 11:54:16 +0200

    Created Camera.get_continuous_readout

--- sstcam_simulation/camera/camera.py
+++ sstcam_simulation/camera/camera.py
@@ -25,3 +25,47 @@ class Camera:
     def continuous_sample_width(self):
         """Read-only. Unit: nanosecond"""
         return CONTINUOUS_SAMPLE_WIDTH
+
+    @property
+    def continuous_time_axis(self):
+        """Time axis for the continuous readout. Unit: nanosecond"""
+        return np.arange(0, self.waveform_length, CONTINUOUS_SAMPLE_WIDTH)
+
+    def get_continuous_readout(self, pixel, time, charge):
+        """
+        Obtain the sudo-continuous readout from the camera for the given
+        photoelectrons (signal and background) in this event
+
+        Parameters
+        ----------
+        pixel : ndarray
+            Array specifying the pixel which contains each photoelectron
+            Shape: (n_photoelectrons)
+        time : ndarray
+            Array specifying the time of arrival for each photoelectron
+            Shape: (n_photoelectrons)
+        charge : ndarray
+            Array specifying the charge reported for each photoelectron
+            Shape: (n_photoelectrons)
+
+        Returns
+        -------
+        convolved : ndarray
+            Array emulating continuous readout from the camera, with the
+            photoelectrons convolved with the reference pulse shape
+            Shape: (n_pixels, n_continuous_readout_samples)
+        """
+        # Samples corresponding to the photoelectron time
+        sample = (time / self.continuous_sample_width).astype(np.int)
+
+        # Add photoelectrons to the readout array
+        n_samples = self.continuous_time_axis.size
+        continuous_readout = np.zeros((self.n_pixels, n_samples))
+        np.add.at(continuous_readout, (pixel, sample), charge)
+
+        # Convolve with the reference pulse shape
+        #  TODO: remove bottleneck
+        pulse = self.reference_pulse.pulse
+        origin = self.reference_pulse.origin
+        convolved = convolve1d(continuous_readout, pulse, mode="constant", origin=origin)
+        return convolved
