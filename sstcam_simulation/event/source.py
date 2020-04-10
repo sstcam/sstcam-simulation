16acd96b33a8fe5109dee681cee6c9e96f0e7b85
Jason Watson <jason.jw@live.co.uk>
2020-04-10 09:47:53 +0200

    Create get_uniform_illumination

--- sstcam_simulation/event/source.py
+++ sstcam_simulation/event/source.py
@@ -42,12 +42,9 @@ class PhotoelectronSource:
         """
         rng = np.random.default_rng(seed=self.seed)
 
+        # Number of NSB photoelectrons per pixel in this event
         length = self.camera.continuous_readout_length
         n_pixels = self.camera.pixel.n_pixels
-        time_axis = self.camera.continuous_time_axis
-        spectrum = self.camera.photoelectron_spectrum
-
-        # Number of NSB photoelectrons per pixel in this event
         avg_photons_per_waveform = rate * 1e6 * length * 1e-9
         n_nsb_per_pixel = rng.poisson(avg_photons_per_waveform, n_pixels)
 
@@ -56,17 +53,56 @@ class PhotoelectronSource:
 
         # Uniformly distribute NSB photoelectrons in time across waveform
         n_photoelectrons = nsb_pixel.size
+        time_axis = self.camera.continuous_time_axis
         nsb_time = rng.choice(time_axis, size=n_photoelectrons)
 
         # Get the charge reported by the photosensor (Inverse Transform Sampling)
+        spectrum = self.camera.photoelectron_spectrum
         nsb_charge = rng.choice(spectrum.x, size=n_photoelectrons, p=spectrum.pdf)
 
         return Photoelectrons(pixel=nsb_pixel, time=nsb_time, charge=nsb_charge)
+
+    def get_uniform_illumination(self, time, illumination, pulse_width=0):
+        """
+        Simulate the camera being illuminated by a uniform light (which already
+        accounts for the focal plane curvature).
+
+        Parameters
+        ----------
+        time : float
+            Arrival time of the light at the focal plane
+        illumination : float
+            Average illumination in number of photoelectrons
+        pulse_width : float
+            Width of the pulse from the illumination source
+
+        Returns
+        -------
+        Photoelectrons
+            Container for the photoelectron arrays
+        """
+        rng = np.random.default_rng(seed=self.seed)
+
+        # Poisson fluctuation of photoelectrons
+        n_pixels = self.camera.pixel.n_pixels
+        n_pe_per_pixel = rng.poisson(illumination, n_pixels)
+
+        # Pixel containing each photoelectron
+        pixel = np.repeat(np.arange(n_pixels), n_pe_per_pixel)
+
+        # Time of arrival for each photoelectron
+        n_photoelectrons = pixel.size
+        time = rng.normal(time, pulse_width, n_photoelectrons)
+
+        # Get the charge reported by the photosensor (Inverse Transform Sampling)
+        spectrum = self.camera.photoelectron_spectrum
+        charge = rng.choice(spectrum.x, size=n_photoelectrons, p=spectrum.pdf)
+
+        return Photoelectrons(pixel=pixel, time=time, charge=charge)
+
 
     # def get_cherenkov_shower(self):
     #     pass
     #     # return charge and time
     #
-    # def get_uniform_illumination(self):
-    #     pass
-    #     # return charge and time
+
--- sstcam_simulation/event/tests/test_source.py
+++ sstcam_simulation/event/tests/test_source.py
@@ -1,5 +1,6 @@
 from sstcam_simulation.camera import Camera, PixelMapping
 from sstcam_simulation.event import PhotoelectronSource
+import numpy as np
 
 
 def test_photoelectron_source():
@@ -19,6 +20,20 @@ def test_get_nsb():
     assert (nsb.charge > 0).all()
 
 
+def test_get_uniform_illumination():
+    pixel_mapping = PixelMapping(n_pixels=2)
+    camera = Camera(pixel=pixel_mapping)
+    simulator = PhotoelectronSource(camera, seed=1)
+    pe = simulator.get_uniform_illumination(time=40, illumination=100, pulse_width=2)
+    assert (pe.pixel.size == pe.time.size) & (pe.pixel.size == pe.charge.size)
+    assert (pe.pixel == 0).sum() == 100
+    assert (pe.pixel == 1).sum() == 94
+    assert (pe.time > 0).all()
+    assert (pe.charge > 0).all()
+    np.testing.assert_allclose(pe.time.mean(), 40, rtol=1e-1)
+    np.testing.assert_allclose(pe.time.std(), 2, rtol=1e-1)
+
+
 def test_seed():
     camera = Camera()
 
@@ -30,14 +45,27 @@ def test_seed():
     simulator_6 = PhotoelectronSource(camera, seed=2)
 
     # get_nsb
-    nsb_1 = simulator_1.get_nsb(rate=1000)
-    nsb_2 = simulator_2.get_nsb(rate=1000)
-    nsb_3 = simulator_3.get_nsb(rate=1000)
-    nsb_4 = simulator_4.get_nsb(rate=1000)
-    nsb_5 = simulator_5.get_nsb(rate=1000)
-    nsb_6 = simulator_6.get_nsb(rate=1000)
-    assert nsb_1 != nsb_2
-    assert nsb_2 != nsb_3  # Generator has been progressed
-    assert nsb_3 != nsb_4
-    assert nsb_4 == nsb_5
-    assert nsb_5 != nsb_6
+    sim_1 = simulator_1.get_nsb(rate=1000)
+    sim_2 = simulator_2.get_nsb(rate=1000)
+    sim_3 = simulator_3.get_nsb(rate=1000)
+    sim_4 = simulator_4.get_nsb(rate=1000)
+    sim_5 = simulator_5.get_nsb(rate=1000)
+    sim_6 = simulator_6.get_nsb(rate=1000)
+    assert sim_1 != sim_2
+    assert sim_2 != sim_3  # Generator has been progressed
+    assert sim_3 != sim_4
+    assert sim_4 == sim_5
+    assert sim_5 != sim_6
+
+    # get_uniform_illumination
+    sim_1 = simulator_1.get_uniform_illumination(time=40, illumination=50, pulse_width=2)
+    sim_2 = simulator_2.get_uniform_illumination(time=40, illumination=50, pulse_width=2)
+    sim_3 = simulator_3.get_uniform_illumination(time=40, illumination=50, pulse_width=2)
+    sim_4 = simulator_4.get_uniform_illumination(time=40, illumination=50, pulse_width=2)
+    sim_5 = simulator_5.get_uniform_illumination(time=40, illumination=50, pulse_width=2)
+    sim_6 = simulator_6.get_uniform_illumination(time=40, illumination=50, pulse_width=2)
+    assert sim_1 != sim_2
+    assert sim_2 != sim_3  # Generator has been progressed
+    assert sim_3 != sim_4
+    assert sim_4 == sim_5
+    assert sim_5 != sim_6
