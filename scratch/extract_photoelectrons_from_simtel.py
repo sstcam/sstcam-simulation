8b2ae29185de2b479cff14f60d4d06effbb82762
Jason J Watson <jason.watson@desy.de>
2020-09-17 12:28:22 +0200

    Update scratch/extract_photoelectrons_from_simtel.py to use new io classes

--- scratch/extract_photoelectrons_from_simtel.py
+++ scratch/extract_photoelectrons_from_simtel.py
@@ -1,14 +1,7 @@
+from sstcam_simulation import SimtelReader, PhotoelectronWriter
 import argparse
-from eventio import SimTelFile
 import tables
 from tqdm import tqdm
-
-
-FILTERS = tables.Filters(
-    complevel=5,  # compression medium, tradeoff between speed and compression
-    complib="blosc:zstd",  # use modern zstd algorithm
-    fletcher32=True,  # add checksums to data chunks
-)
 
 
 class EventTable(tables.IsDescription):
@@ -23,54 +16,6 @@ class EventTable(tables.IsDescription):
     h_first_int = tables.Float64Col()
     x_max = tables.Float64Col()
     shower_primary_id = tables.UInt8Col()
-
-
-def add_telescope_table(h5_file, simtel_file):
-    telescope_descriptions = simtel_file.telescope_descriptions
-    tel_id = list(telescope_descriptions.keys())[0]
-    camera_settings = telescope_descriptions[tel_id]['camera_settings']
-    pixel_x = camera_settings['pixel_x']
-    pixel_y = camera_settings['pixel_y']
-
-    group = h5_file.create_group(h5_file.root, "Geometry", "Camera Geometry")
-    h5_file.create_array(group, 'pixel_x', pixel_x, "Pixel X Coordinates")
-    h5_file.create_array(group, 'pixel_y', pixel_y, "Pixel Y Coordinates")
-
-
-def extract_events(path, table, pe_time, pe_pixel):
-    i = 0
-    with SimTelFile(path) as simtel_file:
-        for iev, event in tqdm(enumerate(simtel_file.iter_mc_events())):
-            if 'photoelectrons' not in event:
-                continue
-
-            photoelectrons = event['photoelectrons']
-            mc_shower = event['mc_shower']
-            mc_event = event['mc_event']
-
-            for tel, values in photoelectrons.items():
-                if values['time'].size < 100:
-                    continue
-
-                pe_time.append(values['time'])
-                pe_pixel.append(values['pixel_id'])
-
-                event = table.row
-                event['event_index'] = iev
-                event['telescope_id'] = tel + 1
-                event['n_photoelectrons'] = values['time'].size
-                event['energy'] = mc_shower["energy"]
-                event['alt'] = mc_shower["altitude"]
-                event['az'] = mc_shower["azimuth"]
-                event['core_x'] = mc_event["xcore"]
-                event['core_y'] = mc_event["ycore"]
-                event['h_first_int'] = mc_shower["h_first_int"]
-                event['x_max'] = mc_shower["xmax"]
-                event['shower_primary_id'] = mc_shower["primary_id"]
-                event.append()
-                i += 1
-            if i > 3:
-                break
 
 
 def main():
@@ -88,35 +33,29 @@ def main():
         '-o', '--output', dest='output_path', action='store',
         help='path to store the output HDF5 file'
     )
+    parser.add_argument(
+        '-n', '--n_events', dest='n_events', action='store',
+        help='number of telescope events to store'
+    )
+    parser.add_argument(
+        '-T', dest='only_tiggered_events', action='store_true',
+        help='store only event that produced a telescope trigger in simtelarray'
+    )
     args = parser.parse_args()
     path_list = args.input_paths
     output_path = args.output_path
+    kwargs = dict(
+        disable_remapping=False,
+        only_triggered_events=args.only_triggered_events,
+        n_events=args.n_events,
+    )
 
-    with tables.File(output_path, mode='w', filters=FILTERS) as h5_file:
-
-        # Extract geometry
-        with SimTelFile(path_list[0]) as simtel_file:
-            add_telescope_table(h5_file, simtel_file)
-
-        # Create event table
-        group = h5_file.create_group(h5_file.root, "Data", "Data")
-        table = h5_file.create_table(group, "event", EventTable, "Simtel Events")
-        pe_time = h5_file.create_vlarray(
-            group,
-            "photoelectron_arrival_time",
-            tables.Float64Atom(shape=()),
-            "Arrival time of the photoelectrons"
-        )
-        pe_pixel = h5_file.create_vlarray(
-            group,
-            "photoelectron_arrival_pixel",
-            tables.UInt16Atom(shape=()),
-            "Pixel hit by the photoelectron"
-        )
-
+    with PhotoelectronWriter(output_path, EventTable) as output:
         for path in tqdm(path_list, desc="Processing simtel files"):
-            extract_events(path, table, pe_time, pe_pixel)
-            h5_file.flush()
+            reader = SimtelReader(path, **kwargs)
+            for pe in reader:
+                output.append(pe)
+            output.flush()
 
 
 if __name__ == '__main__':
