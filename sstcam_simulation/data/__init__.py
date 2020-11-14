3a7546820e185eaf5933c530f21a0f23f3a42493
Jason J Watson <jason.watson@desy.de>
2020-11-14 13:59:21 +0100

    Create get_cherenkov_data

--- sstcam_simulation/data/__init__.py
+++ sstcam_simulation/data/__init__.py
@@ -1,5 +1,18 @@
-from os.path import join, dirname
+from os.path import join, dirname, exists
 
 
 def get_data(path):
     return join(dirname(__file__), path)
+
+
+def get_cherenkov_data():
+    gamma_path = get_data("cherenkov/gamma.h5")
+    proton_path = get_data("cherenkov/gamma.h5")
+    if not exists(gamma_path) or not exists(proton_path):
+        msg = '''
+        Cherenkov files have not been downloaded to sstcam_simulation/data/cherenkov. 
+        The files can be downloaded from Nextcloud 
+        https://pcloud.mpi-hd.mpg.de/index.php/f/142621
+        '''
+        raise ValueError(msg)
+    return [gamma_path, proton_path]
