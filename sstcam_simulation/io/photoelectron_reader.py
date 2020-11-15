dcbac93fe2072e9b564d9134c9dea581eb62316d
Jason J Watson <jason.watson@desy.de>
2020-11-15 15:28:50 +0100

    Add __getitem__ and random_event to PhotoelectronReader

--- sstcam_simulation/io/photoelectron_reader.py
+++ sstcam_simulation/io/photoelectron_reader.py
@@ -1,9 +1,10 @@
 from sstcam_simulation import Photoelectrons
+import numpy as np
 import tables
 
 
 class PhotoelectronReader:
-    def __init__(self, path: str):
+    def __init__(self, path: str, seed=None):
         """
         Reader for photoelectrons stored to a HDF5 file
 
@@ -11,8 +12,12 @@ class PhotoelectronReader:
         ----------
         path : str
             Path to the HDF5 file
+        seed : int or tuple
+            Seed for the numpy random number generator.
+            Ensures the reproducibility of an event if you know its seed
         """
         self._file = tables.File(path, mode='r')
+        self._rng = np.random.RandomState(seed=seed)
 
     def __enter__(self):
         return self
@@ -39,3 +44,40 @@ class PhotoelectronReader:
             yield Photoelectrons(
                 pixel=pixel, time=time, charge=charge, metadata=metadata
             )
+
+    def __getitem__(self, item):
+        data = self._file.root.data
+        pixel = data.photoelectron_arrival_pixel[item]
+        time = data.photoelectron_arrival_time[item]
+        charge = data.photoelectron_measured_charge[item]
+        metadata = dict(zip(data.event_metadata.colnames, data.event_metadata[item]))
+        return Photoelectrons(pixel=pixel, time=time, charge=charge, metadata=metadata)
+
+    def random_event(self, poisson_fluctuate=False):
+        """
+        Obtain a random Cherenkov event from the file
+
+        Parameters
+        ----------
+        poisson_fluctuate : bool
+            Also assume the photoelectrons are an "average" representation for
+            this shower, and fluctuate them with a Poisson distribution.
+            Can be used to get a different image for the same event, but is not
+            technically correct.
+
+        Returns
+        -------
+        pe : Photoelectrons
+            Initial photoelectrons generated in a camera upon observing a Cherenkov shower
+        """
+        pe = self[self._rng.randint(0, self.__len__(), 1)[0]]
+        if poisson_fluctuate:
+            repeats = self._rng.poisson(1, len(pe))
+            return Photoelectrons(
+                pixel=np.repeat(pe.pixel, repeats),
+                time=np.repeat(pe.time, repeats),
+                charge=np.ones(repeats.sum()),
+                metadata=pe.metadata
+            )
+        else:
+            return pe
--- sstcam_simulation/io/tests/test_photoelectron_reader.py
+++ sstcam_simulation/io/tests/test_photoelectron_reader.py
@@ -3,9 +3,11 @@ from sstcam_simulation.io.photoelectron_reader import PhotoelectronReader
 from sstcam_simulation.photoelectrons import Photoelectrons
 import tables
 import numpy as np
+import pytest
 
 
-def test_photoelectron_reader(tmp_path):
+@pytest.fixture(scope='function')
+def pe_file(tmp_path):
     path = str(tmp_path / "test.h5")
     class EventTable(tables.IsDescription):
         test = tables.Float64Col()
@@ -31,15 +33,77 @@ def test_photoelectron_reader(tmp_path):
         writer.append(pe_0)
         writer.append(pe_1)
 
+    return path, pe_list
+
+
+def test_init(pe_file):
+    path, pe_list = pe_file
     reader = PhotoelectronReader(path)
     assert reader._file.isopen
     assert len(reader) == 2
     reader.close()
     assert not reader._file.isopen
 
+
+def test_context(pe_file):
+    path, pe_list = pe_file
     with PhotoelectronReader(path) as reader:
         file = reader._file
         assert file.isopen
         for i, pe in enumerate(reader):
             assert pe == pe_list[i]
     assert not file.isopen
+
+
+def test_getitem(pe_file):
+    path, pe_list = pe_file
+    with PhotoelectronReader(path) as reader:
+        assert reader[0] == pe_list[0]
+        assert reader[1] == pe_list[1]
+
+
+def test_random(pe_file):
+    path, pe_list = pe_file
+    pe = []
+    with PhotoelectronReader(path) as reader:
+        for _ in range(10):
+            pe.append(len(reader.random_event()))
+    assert np.array_equal(np.unique(pe), np.unique([len(p) for p in pe_list]))
+
+
+def test_random_poisson_fluctuate(pe_file):
+    path, pe_list = pe_file
+    with PhotoelectronReader(path) as reader:
+        pe1 = len(reader.random_event(poisson_fluctuate=True))
+
+    pe = []
+    for _ in range(10):
+        with PhotoelectronReader(path) as reader:
+            pe.append(len(reader.random_event(poisson_fluctuate=True)))
+
+    assert not (pe1 == np.array(pe)).all()
+
+
+def test_random_seed(pe_file):
+    path, pe_list = pe_file
+    with PhotoelectronReader(path, seed=1) as reader:
+        pe1 = reader.random_event()
+
+    for _ in range(10):
+        with PhotoelectronReader(path, seed=1) as reader:
+            assert pe1 == reader.random_event()
+
+    with PhotoelectronReader(path, seed=2) as reader:
+        assert pe1 != reader.random_event()
+
+    with PhotoelectronReader(path, seed=1) as reader:
+        pe1 = reader.random_event(poisson_fluctuate=True)
+
+    with PhotoelectronReader(path, seed=1) as reader:
+        pe2 = reader.random_event(poisson_fluctuate=True)
+
+    with PhotoelectronReader(path, seed=2) as reader:
+        pe3 = reader.random_event(poisson_fluctuate=True)
+
+    assert pe1 == pe2
+    assert pe2 != pe3
