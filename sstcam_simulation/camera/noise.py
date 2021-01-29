cdb2c1f771db084c3a6d43b7d3f9717c755b9c02
Jason J Watson <jason.watson@desy.de>
2021-01-29 15:56:20 +0100

    Add TemplateNoise class

--- sstcam_simulation/camera/noise.py
+++ sstcam_simulation/camera/noise.py
@@ -1,5 +1,7 @@
+from sstcam_simulation.data import get_data
 from abc import ABCMeta, abstractmethod
 import numpy as np
+from scipy.interpolate import interp1d
 
 __all__ = [
     "ElectronicNoise",
@@ -66,3 +68,91 @@ class GaussianNoise(ElectronicNoise):
     def add_to_readout(self, readout):
         rng = np.random.default_rng(seed=self.seed)
         return rng.normal(readout, self.stddev)
+
+
+class TemplateNoise(ElectronicNoise):
+    default_path = get_data("datasheet/noise_LMH6722_opamp.txt")
+
+    def __init__(self, n_samples, sample_width, filepath=default_path, stddev=1, seed=None):
+        """
+        Noise defined by a template such as that from a datasheet
+
+        Parameters
+        ----------
+        n_samples : int
+            Number of samples in the readout/waveform
+        sample_width : float
+            Width of samples in the readout/waveform (ns)
+        stddev : float
+            Standard deviation of the noise
+            Units: photoelectrons / ns
+        seed : int or tuple
+            Seed for the numpy random number generator.
+            Ensures the reproducibility of an event if you know its seed
+        """
+        self._n_samples = n_samples
+        self._sample_width = sample_width
+        self._filepath = filepath
+        self._stddev = stddev
+        self._seed = seed
+
+        self._frequency, self._v_root = np.loadtxt(filepath, delimiter=',', unpack=True)
+
+        # Find scaling for requested stddev
+        n_samples_long = int(1e7)
+        voltage = self.get_interpolated_voltage(n_samples_long, sample_width)
+        frequency_spectrum = self.get_frequency_spectrum(voltage)
+        noise = self.get_noise(frequency_spectrum, n_samples_long)
+        self.scale = stddev / np.std(noise)
+
+    def get_interpolated_voltage(self, n_samples, sample_width):
+        df = np.fft.fftfreq(n_samples) / sample_width
+        df_positive = df[:len(df)//2]
+        delta_df_positive = df_positive[1] - df_positive[0]
+        f = interp1d(self._frequency, self._v_root)
+        frequency_min = np.min(self._frequency)
+        frequency_max = np.max(self._frequency)
+        frequency_range = frequency_max - frequency_min
+        frequency_interp = np.arange(frequency_min, frequency_max, frequency_range/n_samples)
+        v_root_interp = f(frequency_interp)
+        return v_root_interp * np.sqrt(delta_df_positive)
+
+    def get_frequency_spectrum(self, voltage):
+        rng = np.random.default_rng(seed=self._seed)
+        phi = rng.uniform(0, 2*np.pi, size=voltage.size)  # Randomising phi from 0 to 2pi
+        cplx = np.zeros(voltage.size, dtype=complex)
+        i = np.arange(1, voltage.size//2)
+        cplx.real[i] = voltage[i]*np.cos(phi[i])
+        cplx.imag[i] = -voltage[i]*np.sin(phi[i])
+        cplx.real[-i] = voltage[i]*np.cos(phi[i])
+        cplx.imag[-i] = voltage[i]*np.sin(phi[i])
+        return cplx
+
+    @staticmethod
+    def get_noise(frequency_spectrum, n_samples):
+        return np.fft.ifft(frequency_spectrum) * n_samples * 1e-9  # Convert to Volts
+
+    @staticmethod
+    def get_noise_envelope(noise, sample_len):
+        """
+        Return back to the noise envelope from the simulated noise
+
+        Parameters
+        ----------
+        noise : ndarray
+            Noise component of the waveform
+        sample_len : int
+            Number of samples in the readout
+
+        Returns
+        -------
+        ndarray
+        """
+        spectrum = np.fft.fft(noise*1e9 / sample_len)  # Convert to nV and rescale for FFT
+        return np.abs(spectrum)
+
+    def add_to_readout(self, readout):
+        voltage = self.get_interpolated_voltage(self._n_samples, self._sample_width)
+        frequency_spectrum = self.get_frequency_spectrum(voltage)
+        noise = self.get_noise(frequency_spectrum, self._n_samples)
+        return readout + noise * self.scale
--- sstcam_simulation/camera/tests/test_noise.py
+++ sstcam_simulation/camera/tests/test_noise.py
@@ -1,11 +1,33 @@
-from sstcam_simulation.camera.noise import ElectronicNoise
-import pytest
+from sstcam_simulation.camera.noise import PerfectElectronics, GaussianNoise, TemplateNoise
 import numpy as np
 
 
-@pytest.mark.parametrize("noise_class", ElectronicNoise.__subclasses__())
-def test_noise(noise_class):
-    noise = noise_class()
+def test_perfect_electronics():
+    noise = PerfectElectronics()
     readout = np.ones((2048, 128))
     noisy = noise.add_to_readout(readout)
     assert noisy.shape == readout.shape
+
+
+def test_gaussian_noise():
+    noise = GaussianNoise(stddev=2)
+    readout = np.ones((2048, 128))
+    noisy = noise.add_to_readout(readout)
+    assert noisy.shape == readout.shape
+    np.testing.assert_allclose(noisy.std(), 2, rtol=1e-2)
+
+
+def test_template_noise():
+    noise = TemplateNoise(1280, 1e-9, stddev=2)
+    readout = np.ones((2048, 1280))
+    noisy = noise.add_to_readout(readout)
+    assert noisy.shape == readout.shape
+    np.testing.assert_allclose(noisy.std(), 2, rtol=1e-2)
+
+
+def test_template_noise_continuous_readout():
+    noise = TemplateNoise(1280*4, 0.25e-9, stddev=2)
+    readout = np.ones((2048, 1280*4))
+    noisy = noise.add_to_readout(readout)
+    assert noisy.shape == readout.shape
+    np.testing.assert_allclose(noisy.std(), 2, rtol=1e-2)
