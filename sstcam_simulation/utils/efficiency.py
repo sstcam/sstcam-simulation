37cf3fdaf0d3c033358969cf2d6631b83f119a80
Jason J Watson <jason.watson@desy.de>
2021-02-05 15:19:02 +0100

    Created CameraEfficiency

---
+++ sstcam_simulation/utils/efficiency.py
@@ -0,0 +1,166 @@
+from sstcam_simulation.data import get_data
+import numpy as np
+import pandas as pd
+from numba import njit
+from astropy import units as u
+
+NSB_FLUX_UNIT = 1/(u.cm**2 * u.ns * u.sr)
+NSB_DIFF_FLUX_UNIT = NSB_FLUX_UNIT / u.nm
+
+
+@njit(fastmath=True)
+def _pixel_active_solid_angle_nb(pixel_diameter, focal_length):
+    pixel_area = pixel_diameter**2
+    equivalent_circular_area = pixel_diameter**2 / 4 * np.pi
+    angle_pixel = pixel_diameter / focal_length
+    area_ratio = pixel_area / equivalent_circular_area
+    return 2 * np.pi * (1.0 - np.cos(0.5 * angle_pixel)) * area_ratio
+
+
+@njit(fastmath=True)
+def _integrate(wavelength, nsb_diff_flux, wavelength_min, wavelength_max):
+    within = (wavelength >= wavelength_min) & (wavelength <= wavelength_max)
+    nsb_within = nsb_diff_flux[within]
+    return np.sum(nsb_within) - 0.5 * (nsb_within[0] + nsb_within[-1])
+
+
+class CameraEfficiency:
+    def __init__(self, path=get_data("datasheet/p4eff_ASTRI-CHEC.lis")):
+        columns = [
+            "wavelength",
+            "eff",
+            "eff+atm.trans.",
+            "q.e.",
+            "ref.",
+            "masts",
+            "filter",
+            "funnel",
+            "atm.trans.",
+            "Ch.light",
+            "NSB",
+            "atm.corr.",
+            "NSB site",
+            "NSB site*eff",
+            "NSB B&E",
+            "NSB B&E*eff",
+        ]
+        self._df = pd.read_csv(path, delimiter=r"\s+", names=columns)
+
+        #TODO: pass via arguments and interp
+
+        self.wavelength = u.Quantity(self._df['wavelength'], 'nm')
+        self._nsb_diff_flux = u.Quantity(self._df['NSB site'], '10^9 / (nm s m^2 sr)')
+        self._atmospheric_transmissivity = u.Quantity(self._df['atm.trans.'].values, '1/nm')
+
+        self.telescope_transmissivity = self._df['masts'].values[0]
+        self.mirror_reflectivity = self._df['ref.'].values
+        self.window_transmissivity = self._df['filter'].values
+        self.pde = self._df['q.e.'].values
+
+        self.mirror_area = u.Quantity(7.931, 'm2')
+        self.pixel_diameter = u.Quantity(0.0062, 'm')
+        self.focal_length = u.Quantity(2.152, 'm/radian')
+
+        self._nsb_flux_300_650 = self._integrate_nsb(
+            self._nsb_diff_flux_on_ground, u.Quantity(300, u.nm), u.Quantity(650, u.nm)
+        )
+
+    @property
+    def _pixel_active_solid_angle(self):
+        solid_angle = _pixel_active_solid_angle_nb(
+            self.pixel_diameter.to_value(u.m),
+            self.focal_length.to_value(u.m/u.radian)
+        )
+        return u.Quantity(solid_angle, u.sr)
+
+    @u.quantity_input
+    def scale_pde(self, wavelength: u.nm, pde_at_wavelength):
+        self.pde *= pde_at_wavelength / np.interp(wavelength, self.wavelength, self.pde)
+
+    @u.quantity_input
+    def _integrate_nsb(self, nsb_diff_flux, wavelength_min: u.nm, wavelength_max: u.nm):
+        integral = _integrate(
+            self.wavelength.to_value(u.nm),
+            nsb_diff_flux.to_value(NSB_DIFF_FLUX_UNIT),
+            wavelength_min.to_value(u.nm),
+            wavelength_max.to_value(u.nm),
+        )
+        return u.Quantity(integral, NSB_FLUX_UNIT)
+
+    @property
+    def _nsb_diff_flux_on_ground(self):
+        return self._nsb_diff_flux
+
+    @property
+    def _nsb_diff_flux_at_camera(self):
+        f = self.telescope_transmissivity * self.mirror_reflectivity
+        return self._nsb_diff_flux_on_ground * f
+
+    @property
+    def _nsb_diff_flux_at_pixel(self):
+        return self._nsb_diff_flux_at_camera * self.window_transmissivity
+
+    @property
+    def _nsb_diff_flux_inside_pixel(self):
+        return self._nsb_diff_flux_at_pixel * self.pde
+
+    @property
+    def _nsb_flux_inside_pixel(self):
+        wl_min = u.Quantity(200, u.nm)
+        wl_max = u.Quantity(999, u.nm)
+        return self._integrate_nsb(self._nsb_diff_flux_inside_pixel, wl_min, wl_max)
+
+    @property
+    def _nsb_rate_inside_pixel(self):
+        rate = self._nsb_flux_inside_pixel * self._pixel_active_solid_angle * self.mirror_area
+        return rate
+
+    @u.quantity_input
+    def get_scaled_nsb_rate(self, nsb_flux: NSB_FLUX_UNIT):
+        scale = nsb_flux / self._nsb_flux_300_650
+        return self._nsb_rate_inside_pixel * scale
+
+    @property
+    def nominal_nsb_rate(self):
+        return self.get_scaled_nsb_rate(u.Quantity(0.24, NSB_FLUX_UNIT))
+
+    @property
+    def high_nsb_rate(self):
+        return self.get_scaled_nsb_rate(u.Quantity(4.3, NSB_FLUX_UNIT))
+
+    @u.quantity_input
+    def _integrate_cherenkov(self, cherenkov_diff_flux, wavelength_min: u.nm, wavelength_max: u.nm):
+        integral = _integrate(
+            self.wavelength.to_value(u.nm),
+            cherenkov_diff_flux.to_value(1 / u.nm),
+            wavelength_min.to_value(u.nm),
+            wavelength_max.to_value(u.nm),
+        )
+        return integral
+
+    @property
+    def _cherenkov_diff_flux_on_ground(self):
+        ref_wavelength = u.Quantity(400, u.nm)
+        return (ref_wavelength / self.wavelength)**2 * self._atmospheric_transmissivity
+
+    @property
+    def _cherenkov_diff_flux_at_camera(self):
+        f = self.telescope_transmissivity * self.mirror_reflectivity
+        return self._cherenkov_diff_flux_on_ground * f
+
+    @property
+    def _cherenkov_diff_flux_at_pixel(self):
+        return self._cherenkov_diff_flux_at_camera * self.window_transmissivity
+
+    @property
+    def _cherenkov_diff_flux_inside_pixel(self):
+        return self._cherenkov_diff_flux_at_pixel * self.pde
+
+    @property
+    def effective_cherenkov_pde(self):
+        flux = self._cherenkov_diff_flux_at_pixel
+        wl_min = u.Quantity(200, u.nm)
+        wl_max = u.Quantity(999, u.nm)
+        n_photons = self._integrate_cherenkov(flux, wl_min, wl_max)
+        n_pe = self._integrate_cherenkov(flux * self.pde, wl_min, wl_max)
+        return n_pe / n_photons
---
+++ sstcam_simulation/utils/tests/test_efficiency.py
@@ -0,0 +1,66 @@
+from sstcam_simulation.utils.efficiency import CameraEfficiency
+import numpy as np
+from astropy import units as u
+
+
+def test_prod4_pixel_active_solid_angle():
+    cam_eff = CameraEfficiency()
+    pixel_active_solid_angle = cam_eff._pixel_active_solid_angle.to_value("μsr")
+    np.testing.assert_allclose(pixel_active_solid_angle, 8.3, rtol=1e-3)
+
+
+def test_scale_pde():
+    cam_eff = CameraEfficiency()
+    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.5)
+    pde_at_wavelength = cam_eff.pde[cam_eff.wavelength == 410]
+    np.testing.assert_allclose(pde_at_wavelength, 0.5, rtol=1e-2)
+    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.25)
+    pde_at_wavelength = cam_eff.pde[cam_eff.wavelength == 410]
+    np.testing.assert_allclose(pde_at_wavelength, 0.25, rtol=1e-2)
+
+
+def test_prod4_nsb_rate_inside_pixel():
+    cam_eff = CameraEfficiency()
+    nsb_rate = cam_eff._nsb_rate_inside_pixel.to_value("1/ns")
+    np.testing.assert_allclose(nsb_rate, 0.044, rtol=1e-2)
+
+
+def test_integrate_nsb():
+    cam_eff = CameraEfficiency()
+    nsb_diff_flux = cam_eff._nsb_diff_flux_on_ground
+    nsb_flux = cam_eff._integrate_nsb(nsb_diff_flux, 300 * u.nm, 550 * u.nm)
+    nsb_flux = nsb_flux.to_value("1/(cm² ns sr)")
+    np.testing.assert_allclose(nsb_flux, 0.116, rtol=1e-2)
+    nsb_flux = cam_eff._integrate_nsb(nsb_diff_flux, 300 * u.nm, 650 * u.nm)
+    nsb_flux = nsb_flux.to_value("1/(cm² ns sr)")
+    np.testing.assert_allclose(nsb_flux, 0.263, rtol=1e-2)
+
+
+def test_prod4_nominal_nsb():
+    cam_eff = CameraEfficiency()
+    nsb_rate = cam_eff.nominal_nsb_rate.to_value("1/ns")
+    np.testing.assert_allclose(nsb_rate, 0.04, rtol=1e-2)
+
+
+def test_prod4_high_nsb():
+    cam_eff = CameraEfficiency()
+    nsb_rate = cam_eff.high_nsb_rate.to_value("1/ns")
+    np.testing.assert_allclose(nsb_rate, 0.72, rtol=1e-2)
+
+
+def test_integrate_cherenkov():
+    cam_eff = CameraEfficiency()
+    diff_flux = cam_eff._cherenkov_diff_flux_inside_pixel
+    flux = cam_eff._integrate_cherenkov(diff_flux, 300 * u.nm, 550 * u.nm)
+    np.testing.assert_allclose(flux, 50.12, rtol=1e-2)
+    flux = cam_eff._integrate_cherenkov(diff_flux, 200 * u.nm, 999 * u.nm)
+    np.testing.assert_allclose(flux, 56.66, rtol=1e-2)
+
+
+def test_effective_cherenkov_pde():
+    cam_eff = CameraEfficiency()
+    np.testing.assert_allclose(cam_eff.effective_cherenkov_pde, 0.42, rtol=1e-2)
+    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.5)
+    np.testing.assert_allclose(cam_eff.effective_cherenkov_pde, 0.43, rtol=1e-2)
+    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.25)
+    np.testing.assert_allclose(cam_eff.effective_cherenkov_pde, 0.213, rtol=1e-2)
