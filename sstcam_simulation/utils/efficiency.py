744d4cbfcda32137feed193d06d3e4e9b8565dbf
Jason J Watson <jason.watson@desy.de>
2021-06-18 14:48:31 +0200

    Improve configurability of CameraEfficiency

--- sstcam_simulation/utils/efficiency.py
+++ sstcam_simulation/utils/efficiency.py
@@ -4,9 +4,17 @@ import numpy as np
 import pandas as pd
 from numba import njit
 from astropy import units as u
+import yaml
 
 NSB_FLUX_UNIT = 1/(u.cm**2 * u.ns * u.sr)
 NSB_DIFF_FLUX_UNIT = NSB_FLUX_UNIT / u.nm
+
+
+PROD4_PATH_WINDOW = get_data("datasheet/efficiency/prod4_window.csv")
+PROD4_PATH_PDE = get_data("datasheet/efficiency/prod4_pde.csv")
+PROD4_PATH_ENV = get_data("datasheet/efficiency/environment.csv")
+PROD4_PATH_TEL = get_data("datasheet/efficiency/prod4_astri_telescope.csv")
+PROD4_PATH_QUAN = get_data("datasheet/efficiency/prod4_quantities.yml")
 
 
 @njit(fastmath=True)
@@ -28,8 +36,11 @@ def _integrate(wavelength, nsb_diff_flux, wavelength_min, wavelength_max):
 class CameraEfficiency:
     def __init__(
             self,
-            path_window=get_data("datasheet/efficiency/prod4_window.csv"),
-            path_pde=get_data("datasheet/efficiency/prod4_pde.csv")
+            path_window=PROD4_PATH_WINDOW,
+            path_pde=PROD4_PATH_PDE,
+            path_environment=PROD4_PATH_ENV,
+            path_telescope=PROD4_PATH_TEL,
+            path_quantities=PROD4_PATH_QUAN,
     ):
         """
         Calculate parameters related to the camera efficiency
@@ -52,7 +63,7 @@ class CameraEfficiency:
         wavelength = np.arange(200, 999)
 
         # Read environment arrays
-        df_env = pd.read_csv(get_data("datasheet/efficiency/environment.csv"))
+        df_env = pd.read_csv(path_environment)
         env_wavelength = df_env['wavelength'].values
 
         def interp_env(y):
@@ -67,7 +78,7 @@ class CameraEfficiency:
         self._atmospheric_transmissivity = u.Quantity(atmospheric_transmissivity, '1/nm')
 
         # Read telescope arrays
-        df_tel = pd.read_csv(get_data("datasheet/efficiency/prod4_astri_telescope.csv"))
+        df_tel = pd.read_csv(path_telescope)
         tel_wavelength = df_tel['wavelength'].values
 
         def interp_tel(y):
@@ -86,13 +97,23 @@ class CameraEfficiency:
         df_pde = pd.read_csv(path_pde)
         pde_wavelength = df_pde["wavelength"].values
         pde = df_pde['pde'].values
-        self.pde = np.interp(wavelength, pde_wavelength, pde)
+        self._pde = np.interp(wavelength, pde_wavelength, pde)
+        self._pde_scale = 1
 
         self.mirror_area = u.Quantity(7.931, 'm2')
         self.pixel_diameter = u.Quantity(0.0062, 'm')
         self.pixel_spacing = u.Quantity(0.0063978, 'm')
         self.pixel_fill_factor = (self.pixel_diameter / self.pixel_spacing)**2
         self.focal_length = u.Quantity(2.152, 'm/radian')
+
+        # # Read scalar quantities
+        # with open(path_quantities, 'r') as stream:
+        #     quantities = yaml.safe_load(stream)
+        #
+        # self.pixel_diameter = u.Quantity(quantities["pixel_diameter"], 'm')
+        # self.pixel_fill_factor = quantities["pixel_fill_factor"]
+        # self.focal_length = u.Quantity(quantities["focal_length"], 'm/radian')
+        # self.mirror_area = u.Quantity(quantities["mirror_area"], 'm2')
 
         self.wavelength = u.Quantity(wavelength, 'nm')
 
@@ -117,9 +138,16 @@ class CameraEfficiency:
         )
         return u.Quantity(solid_angle, u.sr)
 
+    @property
+    def pde(self):
+        return self._pde * self._pde_scale
+
     @u.quantity_input
     def scale_pde(self, wavelength: u.nm, pde_at_wavelength):
-        self.pde *= pde_at_wavelength / np.interp(wavelength, self.wavelength, self.pde)
+        self._pde_scale = pde_at_wavelength / np.interp(wavelength, self.wavelength, self.pde)
+
+    def reset_pde_scale(self):
+        self._pde_scale = 1
 
     @u.quantity_input
     def _integrate_nsb(self, nsb_diff_flux, wavelength_min: u.nm, wavelength_max: u.nm):
--- sstcam_simulation/utils/tests/test_efficiency.py
+++ sstcam_simulation/utils/tests/test_efficiency.py
@@ -1,91 +1,99 @@
-from sstcam_simulation.utils.efficiency import CameraEfficiency
+from sstcam_simulation.utils.efficiency import CameraEfficiency, \
+    PROD4_PATH_WINDOW, PROD4_PATH_PDE, PROD4_PATH_ENV, PROD4_PATH_TEL, PROD4_PATH_QUAN
 import numpy as np
 from astropy import units as u
+import pytest
 
 
-def test_prod4_pixel_active_solid_angle():
-    cam_eff = CameraEfficiency()
-    pixel_active_solid_angle = cam_eff._pixel_active_solid_angle.to_value("μsr")
+@pytest.fixture(scope="module")
+def prod4_cam_eff():
+    return CameraEfficiency(
+        path_window=PROD4_PATH_WINDOW,
+        path_pde=PROD4_PATH_PDE,
+        path_environment=PROD4_PATH_ENV,
+        path_telescope=PROD4_PATH_TEL,
+        path_quantities=PROD4_PATH_QUAN,
+    )
+
+
+def test_prod4_pixel_active_solid_angle(prod4_cam_eff):
+    pixel_active_solid_angle = prod4_cam_eff._pixel_active_solid_angle.to_value("μsr")
     np.testing.assert_allclose(pixel_active_solid_angle, 8.3, rtol=1e-3)
 
 
-def test_prod4_pixel_fill_factor():
-    cam_eff = CameraEfficiency()
-    fill_factor = cam_eff.pixel_fill_factor
+def test_prod4_pixel_fill_factor(prod4_cam_eff):
+    fill_factor = prod4_cam_eff.pixel_fill_factor
     np.testing.assert_allclose(fill_factor, 0.939, rtol=1e-3)
 
 
-def test_scale_pde():
-    cam_eff = CameraEfficiency()
-    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.5)
-    pde_at_wavelength = cam_eff.pde[cam_eff.wavelength == 410]
+def test_scale_pde(prod4_cam_eff):
+    original_pde = prod4_cam_eff.pde.copy()
+
+    prod4_cam_eff.scale_pde(u.Quantity(410, u.nm), 0.5)
+    pde_at_wavelength = prod4_cam_eff.pde[prod4_cam_eff.wavelength == 410]
     np.testing.assert_allclose(pde_at_wavelength, 0.5, rtol=1e-2)
-    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.25)
-    pde_at_wavelength = cam_eff.pde[cam_eff.wavelength == 410]
+    prod4_cam_eff.scale_pde(u.Quantity(410, u.nm), 0.25)
+    pde_at_wavelength = prod4_cam_eff.pde[prod4_cam_eff.wavelength == 410]
     np.testing.assert_allclose(pde_at_wavelength, 0.25, rtol=1e-2)
 
+    prod4_cam_eff.reset_pde_scale()
+    np.testing.assert_allclose(prod4_cam_eff.pde, original_pde, rtol=1e-2)
 
-def test_prod4_nsb_rate_inside_pixel():
-    cam_eff = CameraEfficiency()
-    nsb_rate = cam_eff._nsb_rate_inside_pixel.to_value("1/ns")
+
+def test_prod4_nsb_rate_inside_pixel(prod4_cam_eff):
+    nsb_rate = prod4_cam_eff._nsb_rate_inside_pixel.to_value("1/ns")
     np.testing.assert_allclose(nsb_rate, 0.044, rtol=1e-2)
 
 
-def test_integrate_nsb():
-    cam_eff = CameraEfficiency()
-    nsb_diff_flux = cam_eff._nsb_diff_flux_on_ground
-    nsb_flux = cam_eff._integrate_nsb(nsb_diff_flux, 300 * u.nm, 550 * u.nm)
+def test_prod4_integrate_nsb(prod4_cam_eff):
+    nsb_diff_flux = prod4_cam_eff._nsb_diff_flux_on_ground
+    nsb_flux = prod4_cam_eff._integrate_nsb(nsb_diff_flux, 300 * u.nm, 550 * u.nm)
     nsb_flux = nsb_flux.to_value("1/(cm² ns sr)")
     np.testing.assert_allclose(nsb_flux, 0.116, rtol=1e-2)
-    nsb_flux = cam_eff._integrate_nsb(nsb_diff_flux, 300 * u.nm, 650 * u.nm)
+    nsb_flux = prod4_cam_eff._integrate_nsb(nsb_diff_flux, 300 * u.nm, 650 * u.nm)
     nsb_flux = nsb_flux.to_value("1/(cm² ns sr)")
     np.testing.assert_allclose(nsb_flux, 0.263, rtol=1e-2)
 
 
-def test_prod4_nominal_nsb():
-    cam_eff = CameraEfficiency()
-    nsb_rate = cam_eff.nominal_nsb_rate.to_value("1/ns")
+def test_prod4_nominal_nsb(prod4_cam_eff):
+    nsb_rate = prod4_cam_eff.nominal_nsb_rate.to_value("1/ns")
     np.testing.assert_allclose(nsb_rate, 0.04, rtol=1e-2)
 
 
-def test_prod4_high_nsb():
-    cam_eff = CameraEfficiency()
-    nsb_rate = cam_eff.high_nsb_rate.to_value("1/ns")
+def test_prod4_high_nsb(prod4_cam_eff):
+    nsb_rate = prod4_cam_eff.high_nsb_rate.to_value("1/ns")
     np.testing.assert_allclose(nsb_rate, 0.72, rtol=1e-2)
 
 
-def test_prod4_nominal_moonlight():
-    cam_eff = CameraEfficiency()
-    nsb_rate = cam_eff.nominal_moonlight_rate.to_value("1/ns")
+def test_prod4_nominal_moonlight(prod4_cam_eff):
+    nsb_rate = prod4_cam_eff.nominal_moonlight_rate.to_value("1/ns")
     np.testing.assert_allclose(nsb_rate, 0.145, rtol=1e-2)
 
 
-def test_prod4_high_moonlight():
-    cam_eff = CameraEfficiency()
-    nsb_rate = cam_eff.high_moonlight_rate.to_value("1/ns")
+def test_prod4_high_moonlight(prod4_cam_eff):
+    nsb_rate = prod4_cam_eff.high_moonlight_rate.to_value("1/ns")
     np.testing.assert_allclose(nsb_rate, 0.75, rtol=1e-2)
 
 
-def test_prod4_integrate_cherenkov():
-    cam_eff = CameraEfficiency()
-    diff_flux = cam_eff._cherenkov_diff_flux_inside_pixel
-    flux = cam_eff._integrate_cherenkov(diff_flux, 300 * u.nm, 550 * u.nm)
+def test_prod4_integrate_cherenkov(prod4_cam_eff):
+    diff_flux = prod4_cam_eff._cherenkov_diff_flux_inside_pixel
+    flux = prod4_cam_eff._integrate_cherenkov(diff_flux, 300 * u.nm, 550 * u.nm)
     np.testing.assert_allclose(flux, 50.12, rtol=1e-2)
-    flux = cam_eff._integrate_cherenkov(diff_flux, 200 * u.nm, 999 * u.nm)
+    flux = prod4_cam_eff._integrate_cherenkov(diff_flux, 200 * u.nm, 999 * u.nm)
     np.testing.assert_allclose(flux, 56.66, rtol=1e-2)
 
 
-def test_prod4_effective_cherenkov_pde():
-    cam_eff = CameraEfficiency()
-    np.testing.assert_allclose(cam_eff.effective_cherenkov_pde, 0.441, rtol=1e-3)
-    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.5)
-    np.testing.assert_allclose(cam_eff.effective_cherenkov_pde, 0.451, rtol=1e-3)
-    cam_eff.scale_pde(u.Quantity(410, u.nm), 0.25)
-    np.testing.assert_allclose(cam_eff.effective_cherenkov_pde, 0.2253, rtol=1e-3)
+def test_prod4_effective_cherenkov_pde(prod4_cam_eff):
+    np.testing.assert_allclose(prod4_cam_eff.effective_cherenkov_pde, 0.441, rtol=1e-3)
+    prod4_cam_eff.scale_pde(u.Quantity(410, u.nm), 0.5)
+    np.testing.assert_allclose(prod4_cam_eff.effective_cherenkov_pde, 0.451, rtol=1e-3)
+    prod4_cam_eff.scale_pde(u.Quantity(410, u.nm), 0.25)
+    np.testing.assert_allclose(prod4_cam_eff.effective_cherenkov_pde, 0.2207, rtol=1e-3)
+    prod4_cam_eff.reset_pde_scale()
 
 
-def test_prod4_B_TEL_1170_pde():
-    cam_eff = CameraEfficiency()
-    np.testing.assert_allclose(cam_eff._cherenkov_flux_300_550, 168.41, rtol=1e-4)
-    np.testing.assert_allclose(cam_eff._cherenkov_flux_300_550_inside_pixel_bypass_telescope, 69.58, rtol=1e-4)
-    np.testing.assert_allclose(cam_eff.B_TEL_1170_pde, 0.388, rtol=1e-4)
+def test_prod4_B_TEL_1170_pde(prod4_cam_eff):
+    np.testing.assert_allclose(prod4_cam_eff._cherenkov_flux_300_550, 168.41, rtol=1e-4)
+    f = prod4_cam_eff._cherenkov_flux_300_550_inside_pixel_bypass_telescope
+    np.testing.assert_allclose(f, 69.58, rtol=1e-4)
+    np.testing.assert_allclose(prod4_cam_eff.B_TEL_1170_pde, 0.388, rtol=1e-4)
